ad0b3ad3e43aea08ddfe46c6458cacf2bc26bca1
Vinícius Hoyer <vhoyer@live.com>
2018-06-14 00:11:19 +0000

    Fetched for api config

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Movie } from '../interfaces/movie';
+import { TMDbConfig } from '../interfaces/tmdb-config';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements TMDbConfig {
+  images: { base_url: string; secure_base_url: string; backdrop_sizes: string[]; logo_sizes: string[]; poster_sizes: string[]; profile_sizes: string[]; still_sizes: string[]; };
+  change_keys: string[];
   apiKey:string = '00000000000000000000000000000000';
 
-  constructor(public http:Http) { }
+  constructor(public http:Http) {
+    let raw = this.http.get(`https://api.themoviedb.org/3/configuration?api_key=${this.apiKey}`).toPromise();
+    raw.then(data => {
+      let json = data.json();
+      this.images = json.images;
+      this.change_keys = json.change_keys;
+    });
+  }
 
   getData(){
-    let raw = this.http.get(`https://api.themoviedb.org/3/movie/76341?api_key=${this.apiKey}`).toPromise()
-    return raw.then(data => {
-      console.log(data.json());
-      return (data.json() as Movie);
-    });
+    let raw = this.http.get(`https://api.themoviedb.org/3/movie/76341?api_key=${this.apiKey}&language=pt-BR`).toPromise()
+    return raw.then(data => data.json() as Movie);
   }
 }
