1389a4446de72bd3704976ed88833025a6eb5897
Vinícius Hoyer <vhoyer@live.com>
2018-06-16 20:30:54 +0000

    Alteration on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   query:string = '';
 
   constructor(
-    private router:Router
-  ) {}
+    private router:Router,
+    private route:ActivatedRoute
+  ) {} 
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(data => {
+      this.query = data.q;
+    });
+  }
 
   onSearch(){
     this.router.navigate(['/list'], { queryParams: { 'q': this.query } })
